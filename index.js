738d4e8b47611f546c2cd0c5b5bcedc2fd906903
Abraomukas <abraomukas@gmail.com>
2023-01-30 15:22:46 +0400

    feat: install dependencies
    Create POST endpoint

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
 require('dotenv').config();
 
-var app = express();
-
+app.use(fileUpload({ createParentPath: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
 	res.sendFile(process.cwd() + '/views/index.html');
+});
+
+/*
+ * POST
+ * Returns the NAME, TYPE, and SIZE of the file uploaded by the user
+ */
+app.post('/api/fileanalyse', (req, res) => {
+	try {
+		if (!req.files) {
+			res.json({ status: 412, message: 'No file was uploaded!' });
+		} else {
+			let fileName = req.files.name;
+			let fileType = req.files.mimetype;
+			let fileSize = req.files.size;
+
+			res.json({ name: fileName, type: fileType, size: fileSize });
+		}
+	} catch (err) {
+		res.json({ status: 500, message: err });
+	}
 });
 
 const port = process.env.PORT || 3000;
